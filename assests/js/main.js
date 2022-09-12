dd29a1bfcb86966610a3b8470fe559b2ec50d8ba
Nam Nguyễn <20521631@gm.uit.edu.vn>
2022-09-12 10:22:17 +0700

    Update code

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,5 +1,5 @@
+$(".modal").hide();
 $(document).ready(function(){
-    $(".modal").hide();
     console.log("start");
     initEvent();
     loadData();
@@ -7,62 +7,84 @@ $(document).ready(function(){
 });
 var employeeId = null;
 var formMode = "add";
+var pageNumber=1;//
+var pageSize=20;
+var searchEmployee='';
+var positionFilter='';
+var departmentFilter='';
+var totalPage=0;
+var btnPagingOne=1;
+var btnPagingTwo=btnPagingOne+1;
+var btnPagingThree=btnPagingTwo+1;
+var btnPagingFour=btnPagingThree+1;
+
+
+
+
 function initEvent(){
-    // Ẩn hiện form thông tin sinh viên
+    // Hiện form thông tin sinh viên
     $(".dashboard__data-addemployee").on("click", ()=>{
-        formMode="add"
+        formMode="add";
         $(".modal-employee-information").show();
-        $(".container__body-infor input").val(null)
+        $(".container__body-infor input").val(null);
+        $(".container__body-infor select").val(null);
         $.ajax({
             url: "https://cukcuk.manhnv.net/api/v1/Employees/NewEmployeeCode",
             method: "GET",
-            success: function(newEmployeeCode) {
-                $("#txtEmployeeCode").val(newEmployeeCode);
+            success: function(response) {
+                $("#txtEmployeeCode").val(response);
                 $("#txtEmployeeCode").focus();
             }
         });
 
     })
+    //Thông báo các thông tin bắt buộc nhập
      $('input[required]').blur(function() {
-        // Lấy ra value:
         var value = this.value;
-        // Kiểm tra value:
         if (!value) {
-            // ĐẶt trạng thái tương ứng:
-            // Nếu value rỗng hoặc null thì hiển thị trạng thái lỗi:
             $(this).addClass("input--error");
             $(this).attr('title', "Thông tin này không được phép để trống");
         } else {
-            // Nếu có value thì bỏ cảnh báo lỗi:
             $(this).removeClass("input--error");
             $(this).removeAttr('title');
         }
     })
+    //Bỏ lỗi khi nhất dấu x
     $(".modal__container-exit").click(function (){
         $(this).parents(".modal").hide();
+        $('input[required]').removeClass("input--error");
+        $('input[required]').removeAttr('title');
     })
+    //Bỏ lỗi khi nhất dấu Hủy
     $(".modal__container-footer-cancel").click(function (){
         $(this).parents(".modal").hide();
+        $('input[required]').removeClass("input--error");
+        $('input[required]').removeAttr('title');
     })
+    // Ẩn các thông báo
     $(".btnOK").click(function(){
         $(this).parents(".modal").hide();
     })
+    //Chọn nhân viên
     $(document).on('click', 'table#data__EmployeeList tbody tr', function() {
-        // Xóa tất cả các trạng thái được chọn của các dòng dữ liệu khác:
         $(this).siblings().removeClass('row-selected');
-        // In đậm dòng được chọn:
         this.classList.add("row-selected");
         employeeId = $(this).data('id');
     });
+
+    //Xử lí nhấn nút xóa
     $(".btnDelete").click(function(){
         if(employeeId===null)
         {
+            //Hiện thông báo chưa chọn nhân viên
             $(".modal__anouncement--delete").show();
         }
         else{
+            // Hiện thông báo xác nhận xóa
             $(".modal__anouncement--selected-delete").show();
         }
     })
+    //Xử lí xóa nhân viên
     $(".bntOK--Delete").click(function() {
         // Gọi api thực hiện xóa:
         $.ajax({
@@ -70,28 +92,176 @@ function initEvent(){
             url: "https://cukcuk.manhnv.net/api/v1/employees/" + employeeId,
             success: function(response) {
                 $(".modal__anouncement--selected-delete").hide();
-                // Load lại dữ liệu:
-                loadData();
+                // Load lại dữ liệu
+                $(".modal__anouncement--loading").show();
+                    setTimeout(()=>{
+                        loadData();
+                        employeeId=null;
+                        $(".modal__anouncement--loading").hide();
+                    },500)
             }
         });
     });
+    // Nhân bản
+    $(".btnDuplicate").click(function(){
+        if(employeeId===null)
+        {
+            //Hiện thông báo chưa chọn nhân viên
+            $(".modal__anouncement--delete").show();
+        }
+        else{
+            // Hiện thông báo xác nhận nhân bản
+            $(".modal__anouncement--selected-duplicate").show();
+        }
+    })
+    //Xử lí nhân bản khi nhấn OK
+    $(".bntOK--Duplicate").click(function(){
+        let employeeIdData;
+        console.log(employeeId);
+        //Gán thông tin nhân viên vào biên employeeIdData
+        $.ajax({
+            url: "https://cukcuk.manhnv.net/api/v1/Employees/" + employeeId,
+            method: "GET",
+            async: false,
+            success: function(response) 
+            {
+                console.log(response);
+                employeeIdData=response;
+                console.log(employeeIdData.EmployeeCode);
+            }
+        });
+        //Set giá trị EmployeeCode mới
+        $.ajax({
+            url: "https://cukcuk.manhnv.net/api/v1/Employees/NewEmployeeCode",
+            method: "GET",
+            async: false,
+            success: function(newEmployeeCode) {
+                employeeIdData.EmployeeCode=newEmployeeCode;
+            }
+        });
+        console.log(employeeIdData.EmployeeCode);
+        //Post dữ liệu
+        $.ajax({
+            type: "POST",
+            url: "https://cukcuk.manhnv.net/api/v1/Employees",
+            data: JSON.stringify(employeeIdData),
+            dataType: "json",
+            contentType: "application/json",
+            success: function(response) {
+                $(".modal__anouncement--loading").show();
+                setTimeout(()=>{
+                    loadData();
+                    $(".modal__anouncement--loading").hide();
+                    $(".modal__anouncement--add").show();
+                    employeeId=null;
+                },500);
+            }
+        });
+        
+    })
+    //Tìm kiếm nhân viên theo keyword
+    $(".dashboard__filter-search-input").blur(function(){
+        searchEmployee=this.value;
+        loadData();
+    })
+    //Tìm kiếm nhân viên theo departmentId
+    $(".dashboard__filter-department").change(function(){
+        departmentFilter=this.value;
+        loadData();
+    })
+    //Tìm kiếm nhân viên theo positionId
+    $(".dashboard__filter-position").change(function(){
+        positionFilter=this.value;
+        loadData();
+    })
+    //Chuyển trang
+    $(".dashboard__pagination-previous").click(function(){
+        if(pageNumber>1){
+            pageNumber--;
+            if(pageNumber%4==0)
+            {
+                btnPagingOne=(Math.floor(pageNumber/4)-1)*4 +1;
+                btnPagingTwo=btnPagingOne+1;
+                btnPagingThree=btnPagingTwo+1;
+                btnPagingFour=btnPagingThree+1;
+                $('.dashboard__pagination-one').text(btnPagingOne);
+                $('.dashboard__pagination-two').text(btnPagingTwo);
+                $('.dashboard__pagination-three').text(btnPagingThree);
+                $('.dashboard__pagination-four').text(btnPagingFour);
+            }
+            console.log(pageNumber);
+            loadData();
+        }
+    })
+    $(".dashboard__pagination-next").click(function(){
+        if(pageNumber<totalPage){
+            pageNumber++;
+            if(pageNumber%4==1)
+            {
+                btnPagingOne=Math.floor(pageNumber/4)*4 +1;
+                btnPagingTwo=btnPagingOne+1;
+                btnPagingThree=btnPagingTwo+1;
+                btnPagingFour=btnPagingThree+1;
+                $('.dashboard__pagination-one').text(btnPagingOne);
+                $('.dashboard__pagination-two').text(btnPagingTwo);
+                $('.dashboard__pagination-three').text(btnPagingThree);
+                $('.dashboard__pagination-four').text(btnPagingFour);
+            }
+            console.log(pageNumber);
+            loadData();
+        }
+    })
+    $(".dashboard__pagination-first").click(function(){
+       pageNumber=1;
+       loadData();
+    })
+    $(".dashboard__pagination-last").click(function(){
+        pageNumber=totalPage;
+        loadData();
+     })
+    //Click vào số trang
+    $(".btn__paging").click(function () {
+        $(this).siblings().removeClass('dashboard__pagination-current');
+        this.classList.add("dashboard__pagination-current");
+    })
+    
+
+}
     //---------
     // Validate form
     $('input[required]').blur(function() {
-        // Lấy ra value:
+        // Lấy ra value
         let value = this.value;
-        // Kiểm tra value:
+        // Kiểm tra value
         if (!value) {
-            // ĐẶt trạng thái tương ứng:
-            // Nếu value rỗng hoặc null thì hiển thị trạng thái lỗi:
+            //Nếu value bằng null thì thêm cảnh báo lỗi
             $(this).addClass("input--error");
             $(this).attr('title', "Thông tin này không được phép để trống");
         } else {
             // Nếu có value thì bỏ cảnh báo lỗi:
             $(this).removeClass("input--error");
             $(this).removeAttr('title');
+            //Kiểm tra format email nếu input có type là email
+            if(this.type==="email"){
+                let valueEmail=$(this).val();
+                if(!checkEmailFormat(valueEmail))
+                {
+                    $(this).addClass("input--error");
+                    $(this).attr('title', "Vui lòng nhập đúng định dạng email!");
+                } else 
+                {
+                    $(this).removeClass("input--error");
+                    $(this).removeAttr('title');
+                }
+            }
         }
     })
+
+    //Ngày chọn không lớn hiện tại
+    $("input[type=date]").attr("max",formatDate2(Date.now()));
+    
+
+
     //---------------
     //Rút gọn menu
     $(".dasboard__heading__icon").click(()=>{
@@ -100,10 +270,17 @@ function initEvent(){
         $(".dasboard__info__logo-center").toggleClass("disable");
     })
     //--------
-    //Reload
-    $(".dashboard__filter-refresh").click(()=>{
-       loadData()
-    })
+    //Refresh lại dữ liệu
+    $(".dashboard__filter-refresh").click(function(){
+        $(".modal__anouncement--loading").show();
+        setTimeout(()=>{
+            loadData();
+            employeeId=null;
+            $(".modal__anouncement--loading").hide();
+        },500)
+
+    }
+    )
     
     //-------
     //Chỉnh sửa thông tin nhân viên
@@ -117,10 +294,20 @@ function initEvent(){
         data["DateOfBirth"]=formatDate2(data["DateOfBirth"]);
         data["IdentityDate"]=formatDate2(data["IdentityDate"]);
         data["JoinDate"]=formatDate2(data["JoinDate"]);
-        //------
+        switch(data["WorkStatus"]){
+            case "Đang thử việc":
+                data["WorkStatus"]=1;
+                break;
+            case "Đang làm việc":
+                data["WorkStatus"]=2;
+                break;
+            default:
+                break;
+
+        }  
+        //Set value cho input 
         let inputs = $(".container__body-infor input, .container__body-infor select");
         for (const input of inputs) {
-            // Đọc thông tin propValue:
             const propValue = $(input).attr("propValue");
             console.log(propValue)
             if (propValue) {
@@ -131,9 +318,10 @@ function initEvent(){
         }
      })
     //-------
-    //Lưu
+    //Xử lí nút Lưu
     $("#btnSave").click((event)=>{
         event.preventDefault();
+        //Kiểm tra các input bắt buộc nhập
         let validateform=true;
         console.log(employeeId)
         let inputs=$('input[required]')
@@ -149,31 +337,27 @@ function initEvent(){
             saveData();
         }
         else{
+            //Hiện thông báo nhập các input bắt buộc
             $(".modal__anouncement--validate-form").show()
         }
     });
-    //----------
-}
-//SaveData
+
 function saveData() {
-    // Thu thập dữ liệu:
+    //Lấy value từ các input và select
     let inputs = $(".container__body-infor input, .container__body-infor select");
     let employee = {};
-    console.log("Đối tượng Employee trước khi build");
+    console.log("Thông tin biến employee trước khi set");
     console.log(employee);
-    // build object:
+    //Lưu các giá trị của input tương ứng với propValue
     for (const input of inputs) {
-        // Đọc thông tin propValue:
         const propValue = $(input).attr("propValue");
-        // Lấy ra value:
         if (propValue) {
             let value = input.value;
             employee[propValue] = value;
-            
         }
     
     }
-    console.log("Đối tượng Employee sau khi build");
+    console.log("Thông tin Employee sau khi set");
     console.log(employee);
     // Gọi api thực hiện cất dữ liệu:
     if (formMode == "edit") {
@@ -184,11 +368,11 @@ function saveData() {
             dataType: "json",
             contentType: "application/json",
             success: function(response) {
-                //alert("Sửa dữ liệu thành công!");
+                //Hiện thông báo chỉnh sửa thành công
                 $(".modal__anouncement--edit").show()
-                // load lại dữ liệu:
+                // Load lại dữ liệu
                 loadData();
-                // Ẩn form chi tiết:
+                // Ẩn form thông tin chi tiết
                 $(".modal-employee-information").hide();
 
             }
@@ -201,11 +385,11 @@ function saveData() {
             dataType: "json",
             contentType: "application/json",
             success: function(response) {
-                //alert("Thêm mới dữ liệu thành công!");
+                //Hiện thông báo thêm thành công
                 $(".modal__anouncement--add").show()
-                // load lại dữ liệu:
+                // Load lại dữ liệu
                 loadData();
-                // Ẩn form chi tiết:
+                // Ẩn form thông tin chi tiết
                 $(".modal-employee-information").hide();
 
             }
@@ -217,65 +401,87 @@ function saveData() {
 function loadData(){
     $.ajax({
         type: "GET",
-        url: "https://cukcuk.manhnv.net/api/v1/Employees",
+        url: `https://cukcuk.manhnv.net/api/v1/Employees/filter?pageSize=${pageSize}&pageNumber=${pageNumber}&employeeFilter=${searchEmployee}&positionId=${positionFilter}&departmentId=${departmentFilter}`,
         async: false,
         success: function (response) 
         {
-            $("table tbody").empty();
-            let sort = 1;
-            let ths = $("table#data__EmployeeList thead th");
-            for (const employee of response) 
+            if(response)
             {
-                // duyệt từng cột trong tiêu đề:
-                var trElement = $('<tr></tr>');
-                for (const th of ths) 
+                totalPage=Math.ceil(response.TotalRecord/pageSize);//số lượng trang tối đa
+                const data=response.Data;
+                $("table tbody").empty();
+                let sort = 1;
+                let ths = $("table#data__EmployeeList thead th");
+                for (const employee of data) 
                 {
-                    // Lấy ra propValue tương ứng với các cột:
-                    const propValue = $(th).attr("propValue");
-
-                    const format = $(th).attr("format");
-                    // Lấy giá trị tương ứng với tên của propValue trong đối tượng:
-                    let value = null;
-                    let classAlign = "";
-                    if (propValue == "Sort")
+                    // Duyệt từng cột trong tiêu đề:
+                    var trElement = $('<tr></tr>');
+                    for (const th of ths) 
                     {
-                        value = sort;
+                        // Lấy ra propValue tương ứng với các cột
+                        const propValue = $(th).attr("propValue");
+    
+                        const format = $(th).attr("format");
+                        // Lấy giá trị tương ứng với tên của propValue trong đối tượng
+                        let value = null;
+                        let classAlign = "";
+                        if (propValue == "Sort")
+                        {
+                            value = sort;
+                        }
+                        else
+                        {
+                            value = employee[propValue];
+                        }
+                        switch (format) 
+                        {
+                            case "date":
+                                value = formatDate(value);
+                                break;
+                            case "money":
+                                value = formatMoney(value);
+                                classAlign = "text-align-right";
+                                break;
+                            default:
+                                break;
+                        }
+                        if(propValue==="WorkStatus"){
+                            switch(value){
+                                case 1:
+                                    value="Đang thử việc";
+                                    break;
+                                case 2:
+                                    value="Đang làm việc"
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        
+                        // Tạo thHTML:
+                        let thHTML = `<td class='${classAlign}'>${value||""}</td>`;
+    
+                        // Đẩy vào trHMTL:
+                        trElement.append(thHTML);
                     }
-                    else
-                    {
-                        value = employee[propValue];
-                    }
-                    switch (format) 
-                    {
-                        case "date":
-                            value = formatDate(value);
-                            break;
-                        case "money":
-                            //value = Math.round(Math.random(100) * 1000000);
-                            value = formatMoney(value);
-                            classAlign = "text-align-right";
-                            break;
-                        default:
-                            break;
-                    }
-                    
-                    // Tạo thHTML:
-                    let thHTML = `<td class='${classAlign}'>${value||""}</td>`;
-
-                    // Đẩy vào trHMTL:
-                    trElement.append(thHTML);
+                    sort++;
+                    $(trElement).data("id", employee.EmployeeId);
+                    $(trElement).data("entity", employee);
+                    $("table#data__EmployeeList tbody").append(trElement)
                 }
-                sort++;
-                $(trElement).data("id", employee.EmployeeId);
-                $(trElement).data("entity", employee);
-                $("table#data__EmployeeList tbody").append(trElement)
-        }
+            
+            }
+            else{
+                $("table tbody").empty();
+            }
     },
     error: function(res) {
+        $("table tbody").empty();
         console.log(res);
     }
     });
 }
+
 function formatDate(date) {
     try {
         if (date) {
@@ -331,4 +537,9 @@ function formatMoney(money) {
     } catch (error) {
         console.log(error);
     }
+}
+function checkEmailFormat(email) {
+    const re =
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return re.test(email);
 }
